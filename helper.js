c166fcbe52957ce0e767ab18d74df86443f976e4
hta218 <huynhtuananh21895@gmail.com>
2017-11-24 00:04:12 +0700

    initial commit

---
+++ helper.js
@@ -0,0 +1,165 @@
+
+var getSizeToScale = (realImageWidth, realImageHeight) => {
+  var maxWidthToScale = realImageWidth;
+  var maxHeightToScale = realImageHeight;
+  var ratio = 1.5;
+
+  if (realImageWidth > 800 && realImageHeight < 600) {
+    ratio = realImageWidth / 800;
+    maxWidthToScale = 800;
+    maxHeightToScale = realImageHeight / ratio;
+  }
+  else if (realImageWidth < 800 && realImageHeight > 600) {
+    ratio = realImageHeight / 600;
+    maxHeightToScale = 600;
+    maxWidthToScale = realImageWidth / ratio;
+  }
+  else if (realImageWidth > 800 && realImageHeight > 600) {
+    widthRatio = realImageWidth / 800;
+    heightRatio = realImageHeight / 600;
+    if (widthRatio > heightRatio) {
+      maxWidthToScale = 800;
+      maxHeightToScale = realImageHeight / widthRatio;
+    } else {
+      maxHeightToScale = 600;
+      maxWidthToScale = realImageWidth / widthRatio;
+    }
+  }
+
+  var studentNameDistanceToTop = ($(window).height() - maxHeightToScale) / 2 + 10;
+  studentNameDistanceToTop += maxHeightToScale;
+  studentNameDistanceToTop += 'px';
+
+  maxWidthToScale += 'px';
+  maxHeightToScale += 'px';
+
+  return [maxWidthToScale, maxHeightToScale, studentNameDistanceToTop]
+};
+
+var createKeyframes = (left, top, maxWidthToScale, maxHeightToScale) => {
+  // create keyframe animation
+  var KeyFrame = {
+     init: function(){
+       if(!KeyFrame.check)
+       {
+         // set the style and append to head
+         var css = $(
+           `<style>
+              @keyframes move-y {
+                from {
+                  left : ${left};
+                }
+                to {
+                  left : 50%;
+                }
+              }
+              @keyframes move-x {
+                from {
+                  top : ${top};
+                }
+                to {
+                  top : 50%;
+                  transform: translate(-50%, -50%);
+                }
+              }
+              @keyframes scale {
+                from {
+                  width : 225px;
+                  height: 150px;
+                }
+                to {
+                  width : ${maxWidthToScale};
+                  height: ${maxHeightToScale};
+                }
+              }
+
+           </style>`
+         ).appendTo('head');
+         /*
+         make sure you don't carriage return the css inline statement,
+         or else it'll be error as ILLEGAL
+         so u don't keep appending style to head
+         */
+         KeyFrame.check = true;
+       }
+      }
+    }
+
+    return KeyFrame;
+};
+
+var showFoundStudent = (foundStudentId) => {
+  var $foundStudent = $('#student' + foundStudentId);
+  $foundStudent.addClass('found-student');
+
+  var top = $foundStudent.position().top + 'px';
+  var left = $foundStudent.position().left + 'px';
+  var imgSrc = $('#student' + foundStudentId + ' img').attr('src');
+  var name = $('.found-student #student-name').text();
+
+  $('#image-to-get-size').attr('src', imgSrc);
+
+  var realImageWidth = $('#image-to-get-size').width();
+  var realImageHeight = $('#image-to-get-size').height();
+
+  var sizes = getSizeToScale(realImageWidth, realImageHeight);
+
+  var maxWidthToScale = sizes[0];
+  var maxHeightToScale = sizes[1];
+  var studentNameDistanceToTop = sizes[2];
+
+  var $result = $("#result");
+  $foundStudent.css({
+    'animation' : 'disappear 0.1s 1 ease-in forwards'
+  });
+  $('#result img').attr('src', imgSrc);
+
+  $('#layout').css({
+    'display' : 'block'
+  });
+
+  $result.css({
+    'top' : top,
+    'left': left,
+    'display' : 'inline-block'
+  });
+
+  var KeyFrame = createKeyframes(left, top, maxWidthToScale, maxHeightToScale);
+
+  KeyFrame.init();
+  $result.css({
+    'animation' : `move-y 0.3s 1 linear forwards,
+                   move-x 0.3s 1 linear forwards,
+                   scale 0.4s 1 0.3s linear forwards`
+  });
+
+  $('#layout').css({
+    'animation' : 'change-background 0.2s 1 0.3s ease-in forwards'
+  });
+
+  $('#layout h1').text(name).css({
+    'opacity' : '0',
+    'top' : studentNameDistanceToTop
+  });
+  $('#layout h1').css({
+    'animation' : 'appear 0.2s 1 0.4s ease-in forwards'
+  });
+}
+
+var hideFoundStudent = () => {
+  $('#layout').css({
+    'display' : 'none'
+  });
+  $('.found-student img').css({
+    'border' : 'none'
+  });
+  $('.found-student .bug').css({
+    'display' : 'none'
+  });
+  $('.found-student')
+    .css({
+    'opacity' : '1',
+    'animation' : 'none'
+  })
+    .removeClass('found-student');
+}
---
+++ script.js
@@ -0,0 +1,122 @@
+$(document).ready(function() {
+
+  var url = 'https://script.google.com/macros/s/AKfycbwGR4Kpw-1lpyU2lBRfO7RftXNRKyeS_UNCLBZLesA3JhYMjG6D/exec';
+  var $main = $('#main');
+  var students = [];
+  var bannedList = [];
+  var finding = false;
+  var foundStudentId = '';
+
+  // render students
+  $.get(url, function(data, status) {
+    students = data;
+
+    for (var i = 0; i < students.length; i++) {
+      var bugId = 'bug' + i;
+      var studentId = 'student' + i;
+
+      // set bug id and student id
+      $('#student-form .bug').attr('id', bugId);
+      $('#student-form .student').attr('id', studentId);
+
+      // set image src and student name
+      $('#student-form img').attr('src', students[i].image);
+      $('#student-form #student-name').text(students[i].name);
+
+      var $student = $('#student-form').html();
+      $main.append($student);
+    }
+
+    // show student info
+    $('.show-student').on('click', function() {
+      var studentId = $(this).parents('.student').attr('id');
+      var idNumber = Number(studentId.replace('student', ''));
+
+      showFoundStudent(idNumber);
+
+    });
+
+    // remove student from list
+    $('.remove').on('click', function() {
+      var studentId = $(this).parents('.student').attr('id');
+      var idNumber = Number(studentId.replace('student', ''));
+
+      if (bannedList.includes(idNumber)) {
+        bannedList.splice(bannedList.indexOf(idNumber), 1);
+        $('#' + studentId + ' img').animate({
+          'opacity' : '1'
+        }, 200);
+      } else {
+        bannedList.push(idNumber);
+        $('#' + studentId + ' img').animate({
+          'opacity' : '0.3'
+        }, 200);
+      }
+    });
+    // NOTE: The block code 'remove student from list' MUST be put in block 'render student'
+    // because the js event click render before the HTML element .remove render
+  });
+
+  // handle random event
+  $("#random").click(function() {
+    var numbers = []
+    for (var i = 0; i < students.length; i++) {
+      if (!bannedList.includes(i)) {
+        numbers.push(i);
+      }
+    }
+
+    var findStudent = function() {
+      if (!numbers.length) {
+
+        showFoundStudent(foundStudentId);
+
+      } else {
+        $('.bug').hide().css({
+          'opacity' : '0.3'
+        });
+        $('.bug i').css({
+          'opacity' : '0.2'
+        });
+        var num = numbers[Math.floor(Math.random() * numbers.length)]
+        var index = numbers.indexOf(num);
+
+        var bugName = '#bug' + num;
+        var student = '#student' + num;
+
+        if (numbers.length === 1) {
+          foundStudentId = numbers[0]; // get the last item
+        }
+        $(bugName).show().animate({
+          'opacity' : '1'
+        }, 200);
+        $(bugName + ' i').animate({
+          'opacity' : '1'
+        }, 300);
+
+        // remove num from numbers
+        numbers.splice(index, 1);
+
+        setTimeout(findStudent, 200);
+      }
+    }
+
+    if (!finding && numbers.length) {
+      finding = true;
+      findStudent();
+    }
+
+  });
+
+  // return to find other student
+  $('#layout').click(function() {
+    finding = false;
+    hideFoundStudent();
+
+    // remove found-student from bannedList
+    $('#student' + foundStudentId + ' img').animate({
+      'opacity' : '0.3'
+    }, 200);
+    bannedList.push(Number(foundStudentId));
+  });
+});
